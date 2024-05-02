a8102d5add46d59563a6695f52b36e4cebed07e4
Ale <alessandratrotto000@gmail.com>
2024-05-02 16:56:24 +0200

    modifiche di Ale nella welcome page

--- APP/blood/lib/screens/Splash.dart
+++ APP/blood/lib/screens/Splash.dart
@@ -21,7 +21,7 @@ class _SplashState extends State<Splash>{
 
   void _navigatetoHome(context)async{
     await Future.delayed(Duration(seconds: 3),(){});
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => WelcomeScreen()));
   }
 
   @override
