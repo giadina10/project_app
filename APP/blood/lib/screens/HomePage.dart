3cc84344c1e496bb96075cfd47b52e14e4c44672
giadina10 <giada.businaro@studenti.unipd.it>
2024-06-02 11:34:34 +0200

    collegamento calend e provider riuscito

--- APP/blood/lib/screens/HomePage.dart
+++ APP/blood/lib/screens/HomePage.dart
@@ -6,8 +6,6 @@ import 'package:blood/provider/HomeProvider.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:blood/screens/horizontal_week_calendar.dart';
 import 'package:table_calendar/table_calendar.dart'; // Importa il widget del calendario
 
 class HomePage extends StatefulWidget {
@@ -22,6 +20,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String? name;
   int _selIdx = 0;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -40,53 +40,6 @@ class _HomePageState extends State<HomePage> {
     ),
   ];
 
-  Widget _homeContent() {
-    return SingleChildScrollView(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (name != null)
-              Text('Hello, $name!', style: TextStyle(fontSize: 24)),
-            const SizedBox(height: 20),
-            Consumer<HomeProvider>(
-              builder: (context, provider, child) {
-                if (provider.isLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else {
-                  return Column(
-                    children: [
-                      const Text('Ho preso i dati!'),
-                      Text(provider.risultatoalgoritmo),
-                      ElevatedButton(
-                        onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => Stats(provider),) );
-                          //setState(() {
-                           // _selIdx = 1;
-                         // });
-                       },
-                        child: const Text('Go to statistics'),
-                      ),
-                    ],
-                  );
-                }
-              },
-            ),
-            const SizedBox(height: 20),
-            // Aggiungi il calendario qui
-            TableCalendar(
-              firstDay: DateTime.utc(2020, 1, 1),
-              lastDay: DateTime.utc(2030, 12, 31),
-              focusedDay: DateTime.now(),
-              calendarFormat: CalendarFormat.week,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   void initState() {
     super.initState();
@@ -100,36 +53,172 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-     return ChangeNotifierProvider(create: (context) => HomeProvider(), builder: (context,_)=> Scaffold(
-      appBar:
-      _selIdx == 0? AppBar(
-        title: const Text('Impact Data'),
-      ): AppBar(title: const Text('Profile page'), 
-      actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              _toLogin(context);
-               },
-          ),
-        ],
+  Widget _homeContent(HomeProvider provider) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (name != null)
+              Text(
+                'Hello, $name!',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+              ),
+            const SizedBox(height: 20),
+            TableCalendar(
+              firstDay: DateTime.now().subtract(Duration(days: 2)),
+              lastDay: DateTime.now().add(Duration(days: 2)),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+                _handleSelectedDate(provider, selectedDay);
+              },
+              calendarFormat: CalendarFormat.week,
+              onFormatChanged: (format) {},
+              onPageChanged: (focusedDay) {
+                setState(() {
+                  _focusedDay = focusedDay;
+                });
+              },
+              calendarStyle: CalendarStyle(
+                todayDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                selectedDecoration: BoxDecoration(
+                  color: Colors.red,
+                  shape: BoxShape.circle,
+                ),
+                selectedTextStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                todayTextStyle: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              headerStyle: HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
+              daysOfWeekStyle: DaysOfWeekStyle(
+                weekendStyle: TextStyle(color: Colors.red),
+              ),
+              availableCalendarFormats: const {
+                CalendarFormat.week: 'Week',
+              },
+            ),
+            const SizedBox(height: 20),
+            if (_selectedDay == null)
+              Center(
+                child: Text(
+                  'Scegli il giorno in cui vorresti donare',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                ),
+              )
+            else if (provider.isLoading)
+              Center(child: CircularProgressIndicator())
+            else
+              Column(
+                children: [
+                  if (_selectedDay != null) Text('Ho preso i dati per $_selectedDay!'),
+                  if (_selectedDay != null) Text(provider.risultatoalgoritmo),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Stats(provider),
+                        ),
+                      );
+                    },
+                    child: const Text('Go to statistics'),
+                  ),
+                ],
+              ),
+          ],
+        ),
       ),
-      body: _selIdx ==0 ? _homeContent(): ProfilePage(),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color(0xFFf5f7f7),
-        items: navBarItems,
-        currentIndex: _selIdx,
-        onTap: _onItemTapped,
-      ),
-    )
     );
   }
-   _toLogin(BuildContext context) async {
+
+  void _handleSelectedDate(HomeProvider provider, DateTime selectedDay) {
+    DateTime tomorrow = DateTime.now().add(Duration(days: 1));
+    DateTime today = DateTime.now();
+    DateTime dayAfterTomorrow = today.add(Duration(days: 2));
+
+    if (selectedDay.isAfter(tomorrow)) {//è tra due giorni
+      provider.getData(selectedDay.subtract(Duration(days: 5)), selectedDay.subtract(Duration(days: 3)));
+    } else if (selectedDay.isAfter(today) && selectedDay.isBefore(dayAfterTomorrow)) {
+      provider.getData(selectedDay.subtract(Duration(days: 4)), selectedDay.subtract(Duration(days: 2)));
+    } else {
+      provider.getData(selectedDay.subtract(Duration(days: 3)), selectedDay.subtract(Duration(days: 1)));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => HomeProvider(),
+      builder: (context, _) {
+        final provider = Provider.of<HomeProvider>(context);
+        return Scaffold(
+          appBar: _selIdx == 0
+              ? AppBar(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/splashIcon.png',
+                        scale: 8,
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        'Donify',
+                        style: TextStyle(
+                          fontSize: 34,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'CustomFont',
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  backgroundColor: Colors.red, // Imposta il colore di sfondo in rosso
+                )
+              : AppBar(
+                  title: const Text('Profile page'),
+                  actions: [
+                    IconButton(
+                      icon: Icon(Icons.logout),
+                      onPressed: () async {
+                        _toLogin(context);
+                      },
+                    ),
+                  ],
+                ),
+          body: _selIdx == 0 ? _homeContent(provider) : ProfilePage(),
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: const Color(0xFFf5f7f7),
+            items: navBarItems,
+            currentIndex: _selIdx,
+            onTap: _onItemTapped,
+          ),
+        );
+      },
+    );
+  }
+
+  _toLogin(BuildContext context) async {
     final sp = await SharedPreferences.getInstance();
     await sp.clear();
     Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: ((context) => const LoginPage3())));
+      MaterialPageRoute(builder: (context) => const LoginPage3()),
+    );
   }
 }
