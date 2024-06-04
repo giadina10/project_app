ad4f6722575e1519b5deab01c533e91f8bcc9d51
AlessandraTrotto00 <alessandra.trotto@studenti.unipd.it>
2024-06-04 11:51:21 +0200

    modificato il plot dei passi

--- APP/blood/lib/screens/StatisticPage.dart
+++ APP/blood/lib/screens/StatisticPage.dart
@@ -5,7 +5,7 @@ import 'package:blood/provider/HomeProvider.dart';
 import 'package:blood/utils/plot.dart'; // Adjust the import according to your folder structure
 
 class Stats extends StatelessWidget {
-  const Stats(this.provider,{Key? key}) : super(key: key);
+  const Stats(this.provider, {Key? key}) : super(key: key);
   static const routename = 'Stats';
   final HomeProvider provider;
 
@@ -14,43 +14,39 @@ class Stats extends StatelessWidget {
     List<Steps> steps = provider.steps;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Statistics'),
+        title: const Text(
+                'Daily Personal Statistics',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 25),
+        ),
       ),
-      body:
-          // Assuming 'steps' is a List<Steps> available in HomeProvider
-        
-
-           SingleChildScrollView(
-            child: Column(
-              children: [
-                // Container for the plot
-                Container(
-                  padding: const EdgeInsets.all(16.0),
-                  margin: const EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.redAccent),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: SizedBox(
-                    height: 300, // Set the desired height for the plot
-                    child: StepDataPlot(stepData: steps),
-                  ),
-                ),
-                // Add any other content below the plot
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Additional content goes here.',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-                // Add more widgets or content as needed
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Container for the plot
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              margin: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.redAccent, width: 3),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: SizedBox(
+                height: 200, // Set the desired height for the plot
+                child: StepDataPlot(stepData: steps),
+              ),
             ),
-          )
+            // Add any other content below the plot
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Additional content goes here.',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            // Add more widgets or content as needed
+          ],
+        ),
+      ),
     );
-        }
-      
-    
   }
-
+}
