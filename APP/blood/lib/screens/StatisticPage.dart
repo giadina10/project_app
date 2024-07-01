a8ce1822cab3a52090c751f5624fda1273ecbf5b
giadina10 <giada.businaro@studenti.unipd.it>
2024-07-01 09:25:27 +0200

    aggiunta media HR

--- APP/blood/lib/screens/StatisticPage.dart
+++ APP/blood/lib/screens/StatisticPage.dart
@@ -1,9 +1,9 @@
-import 'package:blood/models/heartrate.dart';
-import 'package:blood/models/calories.dart';
-import 'package:blood/models/steps.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:blood/provider/HomeProvider.dart';
+import 'package:blood/models/heartrate.dart';
+import 'package:blood/models/calories.dart';
+import 'package:blood/models/steps.dart';
 import 'package:blood/utils/plotSteps.dart';
 import 'package:blood/utils/plotCalories.dart';
 import 'package:blood/utils/plotHeartRate.dart'; // Import del nuovo widget PlotHeartRate
@@ -20,6 +20,14 @@ class Stats extends StatelessWidget {
     List<Calories> calories = provider.calories;
     List<HeartRate> heartRates = provider.heartrates; // Recupero dei dati del battito cardiaco
 
+    // Calcolo della media dei valori di Heart Rate
+    double averageHeartRate(List<HeartRate> heartRates) {
+      if (heartRates.isEmpty) return 0;
+
+      int sum = heartRates.map((heartRate) => heartRate.value).reduce((a, b) => a + b);
+      return sum / heartRates.length;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -29,6 +37,7 @@ class Stats extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             // Container per il plot degli steps
             Container(
@@ -56,27 +65,30 @@ class Stats extends StatelessWidget {
                 child: CaloriesPlot(caloriesData: calories),
               ),
             ),
-            // Container per il plot dei battiti cardiaci
+            // Container per la media dei battiti cardiaci
             Container(
               padding: const EdgeInsets.all(16.0),
               margin: const EdgeInsets.all(16.0),
               decoration: BoxDecoration(
-                border: Border.all(color: Colors.greenAccent, width: 3),
+                border: Border.all(color: Colors.orangeAccent, width: 3),
                 borderRadius: BorderRadius.circular(15),
               ),
-              child: SizedBox(
-                height: 200,
-                child: HeartRatePlot(heartRateData: heartRates),
+              child: Column(
+                children: [
+                  Text(
+                    'Average Heart Rate',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    averageHeartRate(heartRates).toStringAsFixed(1), // Mostra la media con una cifra decimale
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ],
               ),
             ),
-            // Aggiungere contenuto aggiuntivo sotto il plot
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Additional content goes here.',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
+            // Container per il plot dei battiti cardiaci
+            
             // Aggiungere altri widget o contenuti se necessario
           ],
         ),
